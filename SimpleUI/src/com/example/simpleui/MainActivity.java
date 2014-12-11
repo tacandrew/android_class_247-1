f141608130daf8c362fa7bbea05239be6486d640
godgunman <godgunman@gmail.com>
2014-12-11 20:24:00 +0800

    add send method

--- SimpleUI/src/com/example/simpleui/MainActivity.java
+++ SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -10,55 +10,59 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private EditText editText;
 	private Button button;
 	private CheckBox checkBox;
-	
-    /* (non-Javadoc)
-     * @see android.app.Activity#onCreate(android.os.Bundle)
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText1);
-        button = (Button) findViewById(R.id.button1);
-        checkBox = (CheckBox) findViewById(R.id.checkBox1);
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        button.setText("submit");
-        checkBox.setText("check box");
-        
-        button.setOnClickListener(new OnClickListener() {
-			
+		editText = (EditText) findViewById(R.id.editText1);
+		button = (Button) findViewById(R.id.button1);
+		checkBox = (CheckBox) findViewById(R.id.checkBox1);
+
+		button.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Log.d("debug", "onclike");
+				send();
 			}
 		});
-    }
+	}
 
+	private void send() {
+		String text = editText.getText().toString();
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+		editText.setText("");
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
