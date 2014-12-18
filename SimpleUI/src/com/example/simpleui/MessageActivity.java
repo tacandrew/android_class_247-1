47c5a77fe4e112ee21572c1b3917b4a9db2e3a86
godgunman <godgunman@gmail.com>
2014-12-18 20:43:38 +0800

    use listview to show message history

--- SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -10,32 +10,42 @@ import java.io.InputStreamReader;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MessageActivity extends Activity {
 
 	private static final String FILE_NAME = "history.txt";
-	private TextView textView;
-	
+	private ListView listView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_message);
-	
-		textView = (TextView) findViewById(R.id.textView1);
-		
+
+		listView = (ListView) findViewById(R.id.listView1);
 		String text = getIntent().getStringExtra("text");
 		boolean isChecked = getIntent().getBooleanExtra("checkbox", false);
-		
+
 		writeToFile(text);
-		textView.setText(readFile());		
+		setListViewData();
 	}
-	
+
+	private void setListViewData() {
+		String allText = readFile();
+		String[] messages = allText.split("\n");
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, messages);
+		listView.setAdapter(adapter);
+	}
+
 	private void writeToFile(String text) {
 		try {
 			text += "\n";
-			FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_APPEND);	
+			FileOutputStream fos = openFileOutput(FILE_NAME,
+					Context.MODE_APPEND);
 			fos.write(text.getBytes());
 			fos.close();
 		} catch (FileNotFoundException e) {
@@ -43,22 +53,22 @@ public class MessageActivity extends Activity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	private String readFile() {
-		
+
 		try {
 			FileInputStream fis = openFileInput(FILE_NAME);
 			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-			
+
 			String line = null;
 			String result = "";
-			while( (line = br.readLine()) != null ) {
-				result += line + "\n";				
+			while ((line = br.readLine()) != null) {
+				result += line + "\n";
 			}
 			return result;
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
