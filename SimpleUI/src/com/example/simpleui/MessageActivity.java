826a3d09c08bf2128dbc74b15bbf638994dd10c2
godgunman <godgunman@gmail.com>
2014-12-18 21:36:55 +0800

    complete SimpleAdapter

--- SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -6,7 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,8 +36,10 @@ public class MessageActivity extends Activity {
 		boolean isChecked = getIntent().getBooleanExtra("checkbox", false);
 
 		writeToFile(text);
-		setListViewData();
+//		setListViewData();
+		setListViewData2();
 	}
+	
 
 	private void setListViewData() {
 		String allText = readFile();
@@ -45,19 +49,30 @@ public class MessageActivity extends Activity {
 				android.R.layout.simple_list_item_1, messages);
 		listView.setAdapter(adapter);
 	}
-	
+
 	private void setListViewData2() {
 		String allText = readFile();
 		String[] messages = allText.split("\n");
 		String[] messageDatetime = new String[messages.length];
-		
-		for (int i = 0 ; i < messageDatetime.length;i++) {
+
+		for (int i = 0; i < messageDatetime.length; i++) {
 			messageDatetime[i] = new Date().toString();
 		}
-		
-//		SimpleAdapter adapter = new SimpleAdapter(this, data, resource, from, to);
-//		listView.setAdapter(adapter);
-		
+
+		List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+		for (int i = 0; i < messages.length; i++) {
+			Map<String, String> item = new HashMap<String, String>();
+			item.put("message", messages[i]);
+			item.put("datetime", messageDatetime[i]);
+			data.add(item);
+		}
+
+		String[] from = new String[] { "message", "datetime" };
+		int[] to = new int[] { R.id.messageTextView, R.id.datatimeTextView };
+		SimpleAdapter adapter = new SimpleAdapter(this, data,
+				R.layout.listview_item, from, to);
+		listView.setAdapter(adapter);
+
 	}
 
 	private void writeToFile(String text) {
