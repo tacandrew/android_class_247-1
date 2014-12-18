f0c9361650c1075add3ac8fa8a75eec874dec5d3
godgunman <godgunman@gmail.com>
2014-12-18 19:47:56 +0800

    add writeToFile method

--- SimpleUI/src/com/example/simpleui/MessageActivity.java
+++ SimpleUI/src/com/example/simpleui/MessageActivity.java
@@ -1,11 +1,17 @@
 package com.example.simpleui;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MessageActivity extends Activity {
 
+	private static final String FILE_NAME = "history.txt";
 	private TextView textView;
 	
 	@Override
@@ -19,6 +25,21 @@ public class MessageActivity extends Activity {
 		String text = getIntent().getStringExtra("text");
 		boolean isChecked = getIntent().getBooleanExtra("checkbox", false);
 		
-		textView.setText(text);
+		textView.setText(text);		
+		writeToFile(text);
+	}
+	
+	private void writeToFile(String text) {
+		try {
+			text += "\n";
+			FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_APPEND);	
+			fos.write(text.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
