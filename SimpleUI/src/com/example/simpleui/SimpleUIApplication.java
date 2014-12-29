af811c194de9445894f95e6c450792717f1787e7
godgunman <godgunman@gmail.com>
2014-12-29 20:00:20 +0800

    refine register method

--- SimpleUI/src/com/example/simpleui/SimpleUIApplication.java
+++ SimpleUI/src/com/example/simpleui/SimpleUIApplication.java
@@ -1,8 +1,13 @@
 package com.example.simpleui;
 
+import java.util.List;
+
+import com.parse.FindCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParsePush;
+import com.parse.ParseQuery;
 import com.parse.PushService;
 
 import android.app.Application;
@@ -20,16 +25,32 @@ public class SimpleUIApplication extends Application {
 		ParsePush.subscribeInBackground("all");
 		ParsePush.subscribeInBackground("device_id_" + getDeviceId());
 		Log.d("debug", "app onCreate");
-		
+
 		register();
 	}
 
 	private void register() {
-		ParseObject object = new ParseObject("DeviceId");
-		object.put("deviceId", getDeviceId());
-		object.saveInBackground();
+		
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("DeviceId");
+		query.whereEqualTo("deviceId", getDeviceId());
+		query.findInBackground(new FindCallback<ParseObject>() {
+			
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				if (objects.size() == 0) {
+					ParseObject object = new ParseObject("DeviceId");
+					object.put("deviceId", getDeviceId());
+					object.saveInBackground();
+					
+					ParsePush push = new ParsePush();
+					push.setChannel("all");
+					push.setMessage("new device coming");
+					push.sendInBackground();
+				}
+			}
+		});
 	}
-	
+
 	private String getDeviceId() {
 		return Secure.getString(getContentResolver(), Secure.ANDROID_ID);
 	}
